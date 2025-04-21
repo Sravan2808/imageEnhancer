f695940e17c1e6326d1a23062884e712f1761922
Sravan Surya <vsravansurya@gmail.com>
2025-04-22 00:15:05 +0530

    Added Header , Footer ,Image Upload and Image Preview,Loading Part and completed Ui Part

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center items-center h-full'>
+        <div className='border-4 w-10 h-10 scale-150  animate-spin border-t-transparent rounded-full border-blue-500'></div>
+      
+    </div>
+  )
+}
+
+export default Loading
