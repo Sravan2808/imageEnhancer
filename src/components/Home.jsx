2c8c0f8a47a69786dbc21f390aeada885190cc2e
Sravan Surya <vsravansurya@gmail.com>
2025-04-24 12:07:01 +0530

    Completed Project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import ImageUpload from "./ImageUpload";
 import ImagePreview from "./ImagePreview";
+import { enhancedImageAPI } from "../utils/enchanceImageApi";
 
 const Home = () => {
   const [uploadImage, setUploadImage] = useState(null);
   const [enhancedImage, setEnhancedImage] = useState(null);
   const [loading, setLoading] = useState(false);
-  const UploadImageHandler = (file) => {
+  const UploadImageHandler = async (file) => {
     setUploadImage(URL.createObjectURL(file));
     setLoading(true);
+    try {
+      const enchancedURL = await enhancedImageAPI(file);
+      setEnhancedImage(enchancedURL);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      alert("Error while enhancing the image.Please try again later.");
+    }
   };
   return (
     <>
@@ -16,7 +25,7 @@ const Home = () => {
       <ImagePreview
         loading={loading}
         uploaded={uploadImage}
-        enhanced={enhancedImage}
+        enhanced={enhancedImage?.image}
       />
     </>
   );
